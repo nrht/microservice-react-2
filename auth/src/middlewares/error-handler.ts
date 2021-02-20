62f35a7cedba15230d0f32ff15cc68941106d02d
nrht <i.norihi.472@gmail.com>
2021-02-20 15:05:49 +0900

    initial commit

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction, request } from 'express';
+import { CustomError } from '../errors/custom-error';
+
+
+export const errorHandler = (
+   err: Error, 
+   req: Request, 
+   res: Response, 
+   next: NextFunction
+) => {
+   if (err instanceof CustomError) {
+      console.log(err.serializeErrors)
+      return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+   }
+
+
+   res.status(400).send({
+      errors: [{ message: 'Something went wrong' }]
+   });
+
+};

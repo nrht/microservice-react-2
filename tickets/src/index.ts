050329d9a954ec44c41f2e6496f3356a37d39fc8
nrht <i.norihi.472@gmail.com>
2021-02-23 16:00:33 +0900

    implemented nats-streaming

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -8,7 +9,30 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be definde');
     }
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error('NATS_CLIENT_ID must be definde');
+    }
+    if (!process.env.NATS_URL) {
+        throw new Error('NATS_URL must be definde');
+    }
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error('NATS_CLUSTER_ID must be definde');
+    }
+
     try {
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL,
+        );
+
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connections closed!');
+            process.exit();
+        });
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
+
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@nrht-ms-demo/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -9,6 +11,7 @@ router.post('/api/tickets', requireAuth, [
       body('title').not().isEmpty().withMessage('Title is required'),
       body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
    ], validateRequest, async (req: Request, res: Response) => {
+
       const { title, price } = req.body;
       const ticket = Ticket.build({
          title,
@@ -16,6 +19,13 @@ router.post('/api/tickets', requireAuth, [
          userId: req.currentUser!.id
       });
       await ticket.save();
+      await new TicketCreatedPublisher(natsWrapper.client).publish({
+         id: ticket.id,
+         title: ticket.title,
+         price: ticket.price,
+         userId: ticket.userId
+      });
+
       res.status(201).send(ticket);
    }
 );
